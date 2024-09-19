f07c05b425d0ecd2379efd16ce9d5b1c6d2c6bc9
BoogieWoogie <ademarhaq@gmail.com>
2024-09-19 14:32:08 +0800

    pertemuan ke 2

---
+++ src/entity/data.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author LAB06
+ */
+public class data {
+    private int angka1;
+    private int angka2;
+    
+    public int getAngka1(){
+        return angka1;
+    }
+    public void setAngka1(int angka1){
+        this.angka1 = angka1;
+    }
+    public int getAngka2(){
+        return angka2;
+    }
+    public void setAngka2(int angka2){
+        this.angka2 = angka2;
+    }
+}
